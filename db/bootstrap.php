4938b0e7326bfcfbab3d66130f4b0ed9f328b0b5
paramireze <paramireze@gmail.com>
2016-11-06 11:41:42 -0600

    changes made

--- db/bootstrap.php
+++ db/bootstrap.php
@@ -14,17 +14,25 @@ function dropTable() {
 function buildDB() {
     $databaseConnection = dbConnection();
     $sql = "
-         CREATE TABLE `test_madisonh3_com`.`user` (
+         CREATE TABLE IF NOT EXISTS `test_madisonh3_com`.`user` (
           `id` INT NOT NULL AUTO_INCREMENT,
           `first` VARCHAR(45) NULL,
           `last` VARCHAR(45) NULL,
           `email` VARCHAR(45) NULL,
-          `hashName` VARCHAR(45) NULL
+          `hashName` VARCHAR(45) NULL,
           PRIMARY KEY (`id`),
           UNIQUE INDEX `email_UNIQUE` (`email` ASC));
     ";
 
     do_pdo_query($databaseConnection, $sql, null);
+}
+
+function addUser() {
+    $databaseConnection = dbConnection();
+    $sql = "INSERT IGNORE INTO `test_madisonh3_com`.`user`
+            SET `ensembl_transcript_id` = ‘ENSORGT00000000001′,
+              `transcript_chrom_start` = 12345,
+              `transcript_chrom_end` = 12678";
 }
 
 ?>
--- view/inc_home.php
+++ view/inc_home.php
@@ -1,5 +1,7 @@
 <h1>Hello Everybody. It is good to see you. I want to hear what you do when you're doing the things you do.</h1>
+<div>let me know if I can help in anyway</div>
 <?php
+buildDB();
 $users = getUsers();
 foreach ($users as $user) {
     echo $user['first'] . ' - ' . $user['last'] . '<br />';
