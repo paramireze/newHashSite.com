996ae6865723751156b23fac5a39a04bb5cb8d97
paramireze <paramireze@gmail.com>
2016-11-07 18:23:39 -0600

    added more classes

--- layout/inc_notification.php
+++ layout/inc_notification.php
@@ -1,14 +1,18 @@
 <?php
 
+// delete me later, this will not be used
 $_SESSION['notification']['type'] = 'success';
 $_SESSION['notification']['message'] = 'message';
+
 if (isset($_SESSION['notification'])) {
+
+    // assign locally and unset notification
     $notificationType = $_SESSION['notification']['type'];
     $notificationMessage = $_SESSION['notification']['message'];
     unset($_SESSION['notification']);
-    echo "<div class='alert alert-$notificationType'>
+
+    echo "
+    <div class='alert alert-$notificationType'>
         $notificationMessage
     </div>";
-
-
 }
--- view/inc_home.php
+++ view/inc_home.php
@@ -4,42 +4,102 @@
 //buildDB();
 //$users = getUsers();
 
-class user {
+class User {
     protected $id;
+    protected $hashName;
     protected $firstName;
     protected $lastName;
 
-    public function __construct($name) {
-        $this->name = $name;
+    public function __construct($id, $hashName, $firstName, $lastName) {
+        $this->id = null;
+        $this->hashName = $hashName;
+        $this->firstName = $firstName;
+        $this->lastName = $lastName;
+
     }
 
-    public function getName() {
-        return $this->name;
+    public function getHashName() {
+        return $this->hashName;
     }
+
+    public function getFirstName() {
+        return $this->firstName;
+    }
+
+    public function getLastName() {
+        return $this->hashName;
+    }
+
 
     public function getId() {
         return $this->id;
     }
 
-    public function setName($name) {
-        $this->name = $name;
+    public function setHashName($hashName) {
+        $this->name = $hashName;
     }
 
 }
 
-class hash {
+class EventType {
     protected $id;
-    protected $host;
     protected $name;
+    protected $created;
+    protected $lastUpdated;
 
-    public function __construct($user, $name) {
+    public function __construct($name) {
         $this->name = $name;
-        $this->host = $user->getId;
+        $this->created = date("Y-m-d H:i:s");
+        $this->lastUpdated = date("Y-m-d H:i:s");
     }
 }
 
-$bill = new User('bill');
-echo $bill->name();
+class Event {
+    protected $id;
+    protected $hare;
+    protected $description;
+    protected $startDate;
+    protected $created;
+    protected $title;
+    protected $cancel;
 
-//dropTable();
+    public function __construct($hare, $title, $startDate, $description = null) {
+        $this->hare = $hare;
+        $this->title = $title;
+        $this->startDate = $startDate;
+        $this->description = $description;
+        $this->created = date("Y-m-d H:i:s");
+
+    }
+
+    public function getHare () {
+        return $this->hare;
+    }
+
+    public function getTitle() {
+        return $this->title;
+    }
+
+    public function getStartDate() {
+        return $this->startDate;
+    }
+
+    public function getDescription() {
+        return $this->description;
+    }
+}
+
+$nummy = new User(1, 'nummy', 'paul', 'ramirez');
+$fedora = new User(2, 'fedora', 'trisha', 'casey');
+$steamingDogVomit = new User(3, 'steaming dog vomit', 'jack', 'philiac');
+
+$hashEvent1 = new Event($nummy, "Nummy's hash Run", "Aug 5th, 2016", "it will be great!");
+
+echo '<pre>';
+print_r($hashEvent1);
+echo '</pre>';
+
+echo '<pre>';
+print_r($nummy);
+echo '</pre>';
 ?>
