a865ffc32a9ec1ae7095d3a88deb306e3ff54621
paramireze <paramireze@gmail.com>
2016-11-16 19:30:09 -0600

    added function that will search a string for a specific word

--- function/common.php
+++ function/common.php
@@ -16,6 +16,10 @@ function MyAutoload($className){
     include_once(SITE_ROOT . '/class/class.' . $className . '.php');
 }
 
+function containsWord($str, $word) {
+    return !!preg_match('#\b' . preg_quote($word, '#') . '\b#i', $str);
+}
+
 spl_autoload_register('MyAutoload');
 
 ?>
