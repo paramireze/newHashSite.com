24acc5e23392f46e16c0cc76476f178f6471c595
paramireze <paramireze@gmail.com>
2016-11-17 08:28:43 -0600

    created functions for tracking database logging operations

--- db/function.php
+++ db/function.php
@@ -20,6 +20,8 @@ function dbConnection() {
 
 if (!function_exists('do_pdo_query')) {
     function do_pdo_query($db, $query, $query_params = NULL) {
+
+        $dbLog = startLog($query, $query_params);
         // no query parameters
         if (empty($query_params)) {
             $stmt = $db->query($query); 
@@ -39,23 +41,63 @@ if (!function_exists('do_pdo_query')) {
                     $stmt->bindValue($key, $value);
                 }
                 $result = $stmt->execute();
-            }
-            catch (Exception $e) {
+                //$dbLog->rowsAffected = !empty($dbLog) ? $stmt->rowCount() : null;
+            } catch (Exception $e) {
                 $log_msg = 'problem executing query "' . $query . '": ' . $e->getMessage();
                 die( $log_msg);
             }
+
             if (!$result) {
                 $log_msg = 'problem executing query "' . $query . '" with paramater set: ' . print_r($query_params, TRUE);
                 die($log_msg);
             }
         }
 
+        $result = insertDatabaseLog($dbLog);
         $stmt->setFetchMode(PDO::FETCH_OBJ);
         return $stmt;
     }
 }
 
+if (!function_exists('startLog')) {
+    function startLog($query, $params) {
+        $dbLog = null;
+        if (shouldCreateDBLog($query)) {
+            $dbLog = new DatabaseLog("No Note", $query, getParameters($params));
+        }
+        return $dbLog;
+    }
+}
 
+function getParameters($params) {
+
+    $isFirst = true;
+    $result = null;
+    if (!empty($params)) {
+        foreach ($params as $key=>$value) {
+            $result .= $isFirst ? "$key=>$value" : ",$key=>$value"; // concat the string
+            $isFirst = false;
+        }
+    }
+
+    return $result;
+}
+
+function shouldCreateDBLog($query) {
+    return !isDatabaseLogQuery($query) && !isSelectStatement($query);
+}
+
+function isDatabaseLogQuery($query) {
+    return !containsWords($query, "database_logs")? true : false; // Test
+}
+
+function isSelectStatement($query){
+    return strtolower(strtok($query, " ")) == "select"? true : false; // Test
+}
+
+function containsWords($str, $word) {
+    return !!preg_match('#\b' . preg_quote(strtolower($word), '#') . '\b#i', $str);
+}
 
 ?>
 
