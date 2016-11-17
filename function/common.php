abc5ebd76a204301781053a2b0ae7194dd10fc24
paramireze <paramireze@gmail.com>
2016-11-16 20:55:02 -0600

    moved function

--- function/common.php
+++ function/common.php
@@ -12,12 +12,9 @@ function getTimeStamp() {
     return date("Y-m-d H:i:s");
 }
 
+
 function MyAutoload($className){
     include_once(SITE_ROOT . '/class/class.' . $className . '.php');
-}
-
-function containsWord($str, $word) {
-    return !!preg_match('#\b' . preg_quote($word, '#') . '\b#i', $str);
 }
 
 spl_autoload_register('MyAutoload');
