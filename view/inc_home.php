83d2ec885bc4127a739ca3245322c9db45cdc3c8
paramireze <paramireze@gmail.com>
2016-11-17 21:17:02 -0600

    cleaned up home page

--- view/inc_home.php
+++ view/inc_home.php
@@ -2,11 +2,15 @@
 <h3>don't judge me</h3>
 
 <?php
+setUser();
 include SITE_ROOT . '/db/bootstrap.php';
 
 rebuildDB();
-getAllDatabaseLogs();
+$dbLogs = getAllDatabaseLogs();
 
-//$hashEvent1 = new Event($nummy, "Nummy's hash Run", "Aug 5th, 2016", "it will be great!");
+include SITE_ROOT . '/view/databaseLogs/inc_displayLogs.php';
+
+
+
 
 ?>
