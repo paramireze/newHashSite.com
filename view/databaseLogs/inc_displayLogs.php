0d12e76e7cb934d4e22dd299b71129d7e327b057
paramireze <paramireze@gmail.com>
2016-11-17 21:17:15 -0600

    display all db logs in table format

---
+++ view/databaseLogs/inc_displayLogs.php
@@ -0,0 +1,32 @@
+<?php
+
+echo "<table class='table'>
+        <tr>
+            <th>id</th>
+            <th>created</th>
+            <th>createdBy</th>
+            <th>note</th>
+            <th>url</th>
+            <th>sql</th>
+            <th>params</th>
+            <th>server</th>
+            <th>rowsAffected</th>
+            <th>status</th>
+            
+        </tr>
+        ";
+foreach ($dbLogs as $log) {
+    echo "<tr><td>" . $log->id . "</td>
+            <td>" . $log->created . "</td>
+            <td>" . $log->createdBy . "</td>
+            <td>" . $log->note . "</td>
+            <td>" . $log->url . "</td>
+            <td>" . $log->sql . "</td>
+            <td>" . $log->params . "</td>
+            <td>" . $log->server . "</td>
+            <td>" . $log->rowsAffected . "</td>
+            <td>" . $log->status . "</td></tr>";
+}
+echo "
+    </table>";
+?>
