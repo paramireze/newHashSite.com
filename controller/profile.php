75db3d55abab9b0a482cf97e40b82c43cbb9cfff
paramireze <paramireze@gmail.com>
2016-11-24 15:12:27 -0600

    create profile controller

---
+++ controller/profile.php
@@ -0,0 +1,14 @@
+<?php
+function profile($params) {
+    $page_content = null;
+    switch($params[1]) {
+        case null:
+            $page_content = 'view/login/inc_loginFormPage.php';
+            break;
+        case "auth":
+            $page_content = '/formSubmits/login/process.php';
+    }
+    return $page_content;
+}
+
+?>
