f347c9aefd47f1e1eb2d2a42f3ef5b09445a27d9
paramireze <paramireze@gmail.com>
2016-11-23 20:02:59 -0600

    moved includes

--- layout/master.php
+++ layout/master.php
@@ -1,15 +1,4 @@
-<?php
 
-
-// get the db connection and functions
-include $_SERVER['DOCUMENT_ROOT'] . '/db/function.php';
-include $_SERVER['DOCUMENT_ROOT'] . '/db/function/user_functions.php';
-include $_SERVER['DOCUMENT_ROOT'] . '/db/function/database_logs_functions.php';
-
-// site wide functions
-include $_SERVER['DOCUMENT_ROOT'] . '/function/common.php';
-
-?>
 <!doctype html>
 
 <html lang="en">
@@ -22,19 +11,21 @@ include $_SERVER['DOCUMENT_ROOT'] . '/function/common.php';
 
     <!-- bootstrap css/js framework -->
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+
     <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
-    <![endif]-->
 </head>
 
 <body>
-
 <?php
 
     include $_SERVER['DOCUMENT_ROOT'] . '/layout/inc_topNavigation.php';
     include $_SERVER['DOCUMENT_ROOT'] . '/layout/inc_notification.php';
     include $_SERVER['DOCUMENT_ROOT'] . '/' . $page_content;
-    include $_SERVER['DOCUMENT_ROOT'] . '/layout/inc_dataDump.php'; ?>
+    if ($params[0] == "home") {
+        include $_SERVER['DOCUMENT_ROOT'] . '/layout/inc_dataDump.php';
+    }
+?>
 
 </body>
 </html>
