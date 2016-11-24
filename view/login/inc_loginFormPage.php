7c65e0f5689dd579e85fbddbc24b9c3219af1201
paramireze <paramireze@gmail.com>
2016-11-23 20:02:45 -0600

    added form fields for logon page

--- view/login/inc_loginFormPage.php
+++ view/login/inc_loginFormPage.php
@@ -1,3 +1,13 @@
-<?php
-echo 'Hello world from logon page';
-?>
+
+<form class="container">
+  <div class="form-group">
+    <label for="inputHashName">User Name</label>
+    <input type="text" class="form-control" id="inputHashName" placeholder="Hash Name">
+  </div>
+  <div class="form-group">
+    <label for="exampleInputPassword1">Password</label>
+    <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
+  </div>
+
+  <button type="submit" class="btn btn-default">Fuck you, log me in</button>
+</form>
