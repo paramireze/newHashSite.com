81b20e67c76ad47428406d7c954687f266e4fea4
paramireze <paramireze@gmail.com>
2016-12-04 16:08:48 -0600

    refactored conditional statements for logging in

--- process/authentication/logout.php
+++ process/authentication/logout.php
@@ -1,6 +1,12 @@
 <?php
-$username = $_POST['username'];
-echo "Hello " . $username;
+include $_SERVER['DOCUMENT_ROOT'] . '/function/common.php';
 
-header("home");
+if (isLoggedIn()) {
+    $redirect = isset($_SERVER['REQUEST_URI']) ? $_SERVER['REQUEST_URI'] : SITE_URL . 'home';
+    header('location: ' . $redirect);
+    exit();
+}
+
+$page_content = 'view/login/inc_loginFormPage.php';
+include($_SERVER['DOCUMENT_ROOT'] . '/layout/master.php');
 ?>
